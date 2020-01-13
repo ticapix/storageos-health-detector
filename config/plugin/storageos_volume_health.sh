5f74118eaae57c8196914aa6e471d12a6a0d1d69
Pierre Gronlier <pierre@gronlier.fr>
2020-01-13 19:54:16 +0000

    display node status by storageos

--- config/plugin/storageos_volume_health.sh
+++ config/plugin/storageos_volume_health.sh
@@ -7,8 +7,8 @@ readonly UNKNOWN=2
 echo NODE: $NODE_NAME
 echo Connection to $STORAGEOS_HOST using user $STORAGEOS_USERNAME
 
-/storageos version || exit $UNKNOWN
-/storageos volume ls
+/storageos node ls || exit $UNKNOWN
+/storageos volume ls || exit $UNKNOWN
 count=`/storageos volume ls --format '{{ json .Health }}' | uniq | grep -v '^"healthy"$\|^[[:space:]]*$' | wc -l`
 
 if [ "$count" -eq "0" ]; then
