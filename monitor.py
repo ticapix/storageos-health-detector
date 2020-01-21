c18c834d8698c3fb20495e15b1ed1a1ab5cdb82f
Pierre Gronlier <pierre@gronlier.fr>
2020-01-21 20:19:45 +0000

    delete node from storageos cluster when node is deleted from k8s cluster

--- monitor.py
+++ monitor.py
@@ -5,9 +5,12 @@ import time
 import os
 import subprocess
 import json
+from urllib3.exceptions import ReadTimeoutError
 
 STORAGEOS_BIN = '/storageos'
 MANDATORY_ENV_VARS = ['NODE_NAME', 'STORAGEOS_BIN', 'STORAGEOS_USERNAME', 'STORAGEOS_PASSWORD', 'STORAGEOS_HOST']
+
+from kubernetes import client, config, watch
 
 
 class StorageOS:
@@ -19,41 +22,43 @@ class StorageOS:
 
     def _run(self, args, **kwargs):
         print('exec: {} ({})'.format(' '.join(args), kwargs))
-        return subprocess.run(args, **kwargs)
+        return subprocess.run([self.storageos_bin] + args, **kwargs)
 
     def _run_json(self, args, **kwargs):
         return json.loads(self._run(args, capture_output=True, text=True, **kwargs).stdout)
 
     def _wait_for_cordon(self):
-        self._run([self.storageos_bin, 'node', 'cordon', self.node_name])
+        self._run(['node', 'cordon', self.node_name])
         info = {'cordon': False}
         while info['cordon'] == False:
             time.sleep(1)
-            info = self._run_json([self.storageos_bin, 'node', 'inspect', self.node_name])[0]
+            info = self._run_json(['node', 'inspect', self.node_name])[0]
 
     def wait_for_drain(self):
         self._wait_for_cordon()
-        self._run([self.storageos_bin, 'node', 'drain', self.node_name])
+        self._run(['node', 'drain', self.node_name])
         info = {'drain': False, 'volumeStats': {}}
         while info['drain'] == False or sum([v for _, v in info['volumeStats'].items()]) != 0:
             time.sleep(1)
-            info = self._run_json([self.storageos_bin, 'node', 'inspect', self.node_name])[0]
+            info = self._run_json(['node', 'inspect', self.node_name])[0]
 
     def wait_for_uncordon(self):
         self._wait_for_undrain()
-        self._run([self.storageos_bin, 'node', 'uncordon', self.node_name])
+        self._run(['node', 'uncordon', self.node_name])
         info = {'cordon': True}
         while info['cordon'] == True:
             time.sleep(1)
-            info = self._run_json([self.storageos_bin, 'node', 'inspect', self.node_name])[0]
+            info = self._run_json(['node', 'inspect', self.node_name])[0]
 
     def _wait_for_undrain(self):
-        self._run([self.storageos_bin, 'node', 'undrain', self.node_name])
+        self._run(['node', 'undrain', self.node_name])
         info = {'drain': True}
         while info['drain'] == True:
             time.sleep(1)
-            info = self._run_json([self.storageos_bin, 'node', 'inspect', self.node_name])[0]
+            info = self._run_json(['node', 'inspect', self.node_name])[0]
 
+    def delete_node(self, node_name):
+        self._run(['node', 'delete', node_name])
 
 class GracefulKiller:
     kill_now = False
@@ -72,7 +77,6 @@ class GracefulKiller:
         self.kill_now = True
         return
 
-
 if __name__ == '__main__':
     missing_vars = [var for var in MANDATORY_ENV_VARS if var not in os.environ]
     if len(missing_vars):
@@ -85,7 +89,21 @@ if __name__ == '__main__':
     print("installing SIGINT and SIGTERM handlers")
     killer = GracefulKiller(storageos)
     print("waiting to be drained ...")
-    while not killer.kill_now:
-        time.sleep(1)
 
+    # Configs can be set in Configuration class directly or using helper utility
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+    w = watch.Watch()
+    while not killer.kill_now:
+        try:
+            for event in w.stream(v1.list_node, timeout_seconds=3):
+                print("Event: %s %s" % (event['type'], event['object'].metadata.name))
+                # Event: ADDED b2-7.1
+                # Event: MODIFIED b2-7.2
+                # Event: DELETED b2-7.1
+                if event['type'].lower() == "deleted":
+                    storageos.delete_node(event['object'].metadata.name)
+        except ReadTimeoutError as ex:
+            pass
+    w.stop()
     print("End of the program. I was killed gracefully :)")
